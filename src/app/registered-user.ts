f34087248aad542addc0b7657020470e12f4a330
AmasiaNalbandian <amasia.nalbandian@mitel.com>
2021-10-24 02:14:16 -0400

    allow registration to dB

--- src/app/registered-user.ts
+++ src/app/registered-user.ts
@@ -1,9 +1,8 @@
 export class RegisteredUser{
     "email": string;
     "password": string;  
-    "user_id" : string;
-    "role_id" : string;
-    "profile_id" : string;
-    "institution_id" : string;
+    "role_id" : number;
+    "profile_id" : number;
+    "institution_id" : number;
   }
   
