502b9527515685925976928c1cd1dd928adf5de3
AmasiaNalbandian <amasia.nalbandian@mitel.com>
2021-11-11 22:36:22 -0500

    customized user-cards and fix css view profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import { FindABuddyComponent } from './find-a-buddy/find-a-buddy.component';
 import { GuardAuthService } from './services/guard-auth.service';
 import { ViewProfileComponent } from './view-profile/view-profile.component';
 import { ChatComponent } from './chat/chat.component';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
 
 
 const routes: Routes = [
@@ -22,7 +23,7 @@ const routes: Routes = [
   { path: 'register', component: UserRegistrationComponent },
   { path: 'findbuddy', component: FindABuddyComponent, canActivate: [GuardAuthService], pathMatch: 'full'}, // show all path
   { path: 'findbuddy/:id', component: FindABuddyComponent, canActivate: [GuardAuthService]}, // show by tag path
-  { path: 'profile', component: ViewProfileComponent, canActivate: [GuardAuthService], pathMatch: 'full' },
+  { path: 'profile', component: EditProfileComponent, canActivate: [GuardAuthService], pathMatch: 'full' },
   { path: 'profile/:id', component: ViewProfileComponent, canActivate: [GuardAuthService] },
   { path: 'chat', component: ChatComponent }
 ];
