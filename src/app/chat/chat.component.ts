7ead8e714c1c41a5b988cadd0e3ce607a21af02e
Natalie Kawalerczak <nkawalerczak@myseneca.ca>
2021-11-14 19:56:49 -0500

    username added  to user's messages. Needs testing with other users.

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 import { ChatService } from '../services/chat.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-chat',
@@ -10,14 +13,26 @@ export class ChatComponent implements OnInit {
 
   private getMessagesSub: any;
   messages: string[] = [];
+  username: any;
   currentMessage: string;
+  public _token: any;
+  private sub: Subscription = new Subscription();
+  private sub2: Subscription = new Subscription();
 
-  constructor(private chatService: ChatService) { }
+  constructor(private chatService: ChatService, private auth: AuthService, private router: Router) { }
 
   ngOnInit() {
+    this._token = this.auth.readToken();
+    this.sub2 = this.auth.getProfile(this._token.userId).subscribe((data) => {
+      this.username = data.profile_name;
+      this.chatService.sendUsername(this.username);
+      console.log(this.username);
+    });
+
     this.getMessagesSub = this.chatService.getMessages.subscribe((data) => {
       this.messages.push(data);
     });
+
   }
 
   sendMessage() {
