2cdd085a2a3a2d7e05b394dd5ba0d5dd580bf532
AmasiaNalbandian <amasia.nalbandian@mitel.com>
2021-12-03 20:44:42 -0500

    added prod variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,4 +3,7 @@ export const environment = {
   //clientID: "hjxmhsbzgksufo",
   //clientSecret: "e36b848a2abaed80e7f11b1583ffe84613f2b2b6fc9fdd537d6679fea487b548",
   userAPIBase: 'https://campus-buddy-db.herokuapp.com/',
+  socketServer: 'https://cb-chatserver.herokuapp.com/',
+  socketServer2: 'https://socketserver-2.herokuapp.com/',
+  socketServer3: 'https://socketserver3.herokuapp.com/',
 };
