27e9b5edc9360bb4689debd83049c3881b6a0f1a
Natalie Kawalerczak <nkawalerczak@myseneca.ca>
2021-12-07 17:53:10 -0500

    changes

--- src/app/find-a-buddy/find-a-buddy.component.ts
+++ src/app/find-a-buddy/find-a-buddy.component.ts
@@ -17,23 +17,16 @@ export class FindABuddyComponent implements OnInit {
   private id: any;
   private _token: any;
 
-  constructor(
-    private auth: AuthService,
-    private route: ActivatedRoute,
-    private router: Router
-  ) {
+  constructor(private auth: AuthService, private route: ActivatedRoute, private router: Router) {
     this.router.routeReuseStrategy.shouldReuseRoute = () => false;
   }
 
   ngOnInit(): void {
-
     this._token = this.auth.readToken();
     this.sub2 = this.auth.getAllProfiles().subscribe((data) => {
       this.users = data.rows;
 
-      const currentUserId = this.users.findIndex(
-        (user) => user.user_id == this._token.userId
-      );
+      const currentUserId = this.users.findIndex((user) => user.user_id == this._token.userId);
       let newUsers = <any>[];
       if (this.id) {
         for (let user of this.users) {
